82359a0d0d711660191d25849ba94967dc1b4e1d
cmsyog <cmsyog@gmail.com>
2018-12-28 12:13:54 +0700

    config db

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => '192.168.1.50',
+	'username' => 'username',
+	'password' => 'password',
+	'database' => 'generator-password',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
